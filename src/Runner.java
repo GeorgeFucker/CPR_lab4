bd7d83971fbcfcd61be841e582efa6ef5cfd33d8
GeorgeFucker <33426483+GeorgeFucker@users.noreply.github.com>
2018-10-03 09:57:48 +0300

    test version

---
+++ src/Runner.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Runner {
+
+    //id, Марка, Модель, Год выпуска, Цвет, Цена, Регистрационный номер.
+
+    public static void main(String[] args){
+        ArrayList<Car> cars = new ArrayList<Car>();
+
+        cars.add()
+    }
+}
