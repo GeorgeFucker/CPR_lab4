2591f5691558b43a89cab5cb41b28f39b44527a6
Vitaliy <vitalit1398@icloud.com>
2018-10-16 20:37:48 +0300

    Створив класс Car. В классі Runner інстанціював декілька екземплярів класу. Планую зробити автоматичне створення Id та enum  для деяких полів класу Car

---
+++ src/Car.java
@@ -0,0 +1,80 @@
+public class Car {
+    private String id;
+    private String brand;
+    private String model;
+    private int year;
+    private String color;
+    private int price;
+    private long regNumber;
+    public Car(String id, String brand, String model, int year, String color, int price, long regNumber){
+
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.price = price;
+        this.regNumber = regNumber;
+    }
+
+    public Car(){
+
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public long getRegNumber() {
+        return regNumber;
+    }
+
+    public void setRegNumber(long regNumber) {
+        this.regNumber = regNumber;
+    }
+}
